c3ddb00297e2d5177c841fda526876ea5790dbb0
Yagub Shukurov <shukurov23@outlook.com>
2020-01-02 23:43:08 +0100

    UserController for REST

--- src/main/java/com/shukurov23/app/ws/ui/controller/UserController.java
+++ src/main/java/com/shukurov23/app/ws/ui/controller/UserController.java
@@ -1,5 +1,39 @@
 package com.shukurov23.app.ws.ui.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// adding annotation to register this class as RestController
+@RestController
+// http request contains url path
+@RequestMapping("users") //http://localhost:8080/users
+public class UserController {
+	
+	@GetMapping
+	public String getUser() 
+	{
+		return "get user was called";
+	}
+	
+	@PostMapping
+	public String createUser()
+	{
+		return "create user was called";
+	}
+	
+	@PutMapping
+	public String updateUser()
+	{
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser()
+	{
+		return "delete user was called";
+	}
 }
